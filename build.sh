d91dbaed45448f81cd9604068cbb6b2d084ee13e
TOKITA Hiroshi <tokita.hiroshi@gmail.com>
2026-03-25 17:39:45 +0900

    Share the sstate and downloads betweeen AGL and moulin
    Use same sstate-cache and downloaded materials between AGL build
    and moulin.
    
    Signed-off-by: TOKITA Hiroshi <tokita.hiroshi@gmail.com>

--- build.sh
+++ build.sh
@@ -54,6 +54,9 @@ if [ ! -f "external/meta-rcar-demo/firmware/rcar_gen4_pcie.bin" ]; then
     dieif curl -o external/meta-rcar-demo/firmware/rcar_gen4_pcie.bin 'https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/plain/rcar_gen4_pcie.bin?id=e56e0a4c8985ec8559aa7b8a831cb841dc8505e6'
 fi
 
+mkdir -p external/meta-rcar-demo/work_v4hsbc_xen/yocto/common_data/downloads
+mkdir -p external/meta-rcar-demo/work_v4hsbc_xen/yocto/common_data/sstate
+
 # Build flatcar custum image
 if [ "${ENABLE_FLATCAR_BUILD}" = "yes" ]; then
     echo "=== Building Flatcar Container Linux image ==="
